30ab2db71a66ce4cbeeb13be98068fa32b8c14ab
Hanane-AitLakbir <HananeLocal@10.70.0.124>
2014-02-21 08:27:12 +0100

    putRequirement (ModuleCtrl) & setPreference (StudentCtrl)

--- ModulesChoice/grails-app/controllers/moduleschoice/ModuleController.groovy
+++ ModulesChoice/grails-app/controllers/moduleschoice/ModuleController.groovy
@@ -7,23 +7,27 @@ class ModuleController {
 
 	def scaffold = true
 
-	def exportResult(){
-		def Module = Module.getByTitle(params.title)
-		ArrayList<ArrayList<String>> eleves = new ArrayList<ArrayList<String>>(module.getApplications.size());
-
-		for(Application a : module.getApplications()){
-			ArrayList<String> eleve = new ArrayList<String>(4);
-			String name = a.getStudentName();
-			eleve.add(name);
-			String surname = a.getStudentSurname();
-			eleve.add(surname);
-			String choice = a.getChoice();
-			eleve.add(choice);
-			String preference = a.getPreference();
-			eleve.add(preference);
-			eleves.add(eleve);
-		}
+//	def exportResult(){
+//		def Module = Module.findByEnsicaName(params.module)
+//		ArrayList<ArrayList<String>> eleves = new ArrayList<ArrayList<String>>(module.getApplications.size());
+//
+//		for(Application a : module.getApplications()){
+//			ArrayList<String> eleve = new ArrayList<String>(4);
+//			String name = a.getStudentName();
+//			eleve.add(name);
+//			String surname = a.getStudentSurname();
+//			eleve.add(surname);
+//			String choice = a.getChoice();
+//			eleve.add(choice);
+//			String preference = a.getPreference();
+//			eleve.add(preference);
+//			eleves.add(eleve);
+//		}
+//	}
+	
+	def putRequirement(){
+		def module = Module.findByEnsicaName()
+		module.requirements.add(Module.findByEnsicaName(params.module))
 	}
-	//def exp = new Export().exportExcel(module.getTitle(),eleves)
 }
 
